dd5936a03e2a9c9bb694c04fa61c39d6487264e8
naoki hirata <naoki@magic3.org>
2020-01-14 08:10:51 +0900

    エラーメッセージ修正。

--- db-transaction/main.js
+++ db-transaction/main.js
@@ -4,24 +4,31 @@ const UserDb = require('./userDb')
 const userDb = new UserDb()
 
 ;(async () => {
-  const [err] = await userDb.tranBlock()
+  console.log('#トランザクションブロック1を実行')
+  const [err] = await userDb.tranBlock1()
   if (err) throw new Error('トランザクションブロック1でエラー発生')
 
-  const [err3, result3] = await userDb.getUsers()
-  console.log(result3)
-  console.log('main...end....')
+  console.log('#DBの内容を表示')
+  const [err2, result2] = await userDb.getUsers()
+  console.log(result2)
 
-  const [err2] = await userDb.tranBlockToFail1()
-  if (err2) throw new Error('トランザクションブロック2でエラー発生')
-  console.log(err2)
+  console.log('#トランザクションブロック2を実行')
+  const [err3] = await userDb.tranBlock2ToFail()
+  if (err3) {
+    console.log('#DBの内容を表示')
+    const [err4, result4] = await userDb.getUsers()
+    console.log(result4)
 
-  // 正常終了
+    throw new Error('トランザクションブロック2でエラー発生')
+  }
+
+  // 正常終了(ここには来ない)
   console.log('#正常終了')
   process.exit(0)
 })().catch((err) => { // reject()実行でこちらに来る
   console.error(err)
 
-  // 異常終了
+  // 異常終了(こちらで終了)
   console.log('#異常終了')
   process.exit(1)
 })
--- db-transaction/userDb.js
+++ db-transaction/userDb.js
@@ -6,27 +6,28 @@ class UserDb extends BaseDb {
     return this.execQuery(sql)
   }
 
-  tranBlock () {
-    console.log('start...')
-
+  tranBlock1 () {
     const asyncFunc = async (conn) => {
-      // データクリア
+      console.log('#トランザクションブロック1 - 開始')
+
+      // テーブルクリア
       await conn.execute('DELETE FROM users')
 
       // 初期データ追加
       const values = [['taro', '太郎'], ['hanako', '花子'], ['jiro', '次郎']]
       const sql = 'INSERT INTO users (account, name) VALUES ?'
       await conn.query(sql, [values])
-      console.log('-----end----')
+
+      console.log('#トランザクションブロック1 - 終了')
     }
     return this.execAsyncFunctionWithTran(asyncFunc)
   }
 
-  tranBlockToFail1 () {
-    console.log('#tranFail1 start...')
-
+  tranBlock2ToFail () {
     const asyncFunc = async (conn) => {
-      // データクリア
+      console.log('#トランザクションブロック2 - 開始')
+
+      // テーブルクリア
       await conn.execute('DELETE FROM users')
 
       // 初期データ追加
@@ -38,7 +39,8 @@ class UserDb extends BaseDb {
       const values2 = [['taro', '太郎2'], ['hanako', '花子2'], ['jiro', '次郎2']]
       const sql2 = 'INSERT INTO users (account, name) VALUES ?'
       await conn.query(sql2, [values2])
-      console.log('#tranFail1 -----end----')
+
+      console.log('#トランザクションブロック2 - 終了')
     }
     return this.execAsyncFunctionWithTran(asyncFunc)
   }
