152b9fc1784ec64e89f195e0dedf1251c331773c
naoki hirata <naoki@magic3.org>
2020-01-13 13:12:12 +0900

    データ表示処理追加。

--- db-transaction/main.js
+++ db-transaction/main.js
@@ -4,41 +4,24 @@ const UserDb = require('./userDb')
 const userDb = new UserDb()
 
 ;(async () => {
-  // await userDb.initData()
-  const [err] = await userDb.initData2()
-  if (err) throw new Error('error....')
+  const [err] = await userDb.tranBlock()
+  if (err) throw new Error('トランザクションブロック1でエラー発生')
 
-  console.log(err)
+  const [err3, result3] = await userDb.getUsers()
+  console.log(result3)
   console.log('main...end....')
 
-  const [err2] = await userDb.tranFail1()
-  if (err2) throw new Error('error2....')
+  const [err2] = await userDb.tranBlockFail1()
+  if (err2) throw new Error('トランザクションブロック2でエラー発生')
   console.log(err2)
 
   // 正常終了
-  console.log('正常終了')
+  console.log('#正常終了')
   process.exit(0)
 })().catch((err) => { // reject()実行でこちらに来る
-  console.log('エラー発生')
   console.error(err)
 
   // 異常終了
-  console.log('異常終了')
+  console.log('#異常終了')
   process.exit(1)
 })
-// console.log('#DB接続開始')
-
-// const userDb = new UserDb()
-/*
-  await userDb.getUsers((err, result) => {
-    if (err) {
-      // console.log('取得エラー')
-
-    }
-    // 取得データを表示
-    // console.log('ID=' + result[0].id + ', 名前=' + result[0].name)
-    // console.log(result)
-    // console.log('#DB接続終了')
-  })
-  console.log('#ユーザ取得終了')
-  */
